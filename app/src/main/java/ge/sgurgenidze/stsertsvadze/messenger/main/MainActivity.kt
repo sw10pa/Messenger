45e6f3f028f3be5febcf8a6983d0522242e240e0
Stephane Gurgenidze <v-stephang@microsoft.com>
2021-08-08 20:51:52 +0400

    Configured firebase

--- app/src/main/java/ge/sgurgenidze/stsertsvadze/messenger/main/MainActivity.kt
+++ app/src/main/java/ge/sgurgenidze/stsertsvadze/messenger/main/MainActivity.kt
@@ -11,7 +11,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        openFirstActivity();
+        openFirstActivity()
     }
 
     private fun openFirstActivity() {
