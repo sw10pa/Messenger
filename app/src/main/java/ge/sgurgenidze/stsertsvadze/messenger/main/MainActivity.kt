226bc410eb475a6302db6f808f2654f40571aad0
Stephane Gurgenidze <v-stephang@microsoft.com>
2021-08-08 17:17:25 +0400

    Created login activity

---
+++ app/src/main/java/ge/sgurgenidze/stsertsvadze/messenger/main/MainActivity.kt
@@ -0,0 +1,22 @@
+package ge.sgurgenidze.stsertsvadze.messenger.main
+
+import android.os.Bundle
+import android.content.Intent
+import ge.sgurgenidze.stsertsvadze.messenger.R
+import androidx.appcompat.app.AppCompatActivity
+import ge.sgurgenidze.stsertsvadze.messenger.login.LoginActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        openFirstActivity();
+    }
+
+    private fun openFirstActivity() {
+        val intent = Intent(this, LoginActivity::class.java)
+        startActivity(intent)
+    }
+
+}
