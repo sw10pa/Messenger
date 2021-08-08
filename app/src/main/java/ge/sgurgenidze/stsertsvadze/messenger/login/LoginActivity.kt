7b94259fb5719c0c815154709627ec51e6a78634
Stephane Gurgenidze <v-stephang@microsoft.com>
2021-08-09 01:30:31 +0400

    Created registration activity

--- app/src/main/java/ge/sgurgenidze/stsertsvadze/messenger/login/LoginActivity.kt
+++ app/src/main/java/ge/sgurgenidze/stsertsvadze/messenger/login/LoginActivity.kt
@@ -1,14 +1,22 @@
 package ge.sgurgenidze.stsertsvadze.messenger.login
 
 import android.os.Bundle
+import android.view.View
+import android.content.Intent
 import ge.sgurgenidze.stsertsvadze.messenger.R
 import androidx.appcompat.app.AppCompatActivity
+import ge.sgurgenidze.stsertsvadze.messenger.registration.RegistrationActivity
 
 class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+    }
+
+    fun signUpButtonClicked(view: View) {
+        val intent = Intent(this, RegistrationActivity::class.java)
+        startActivity(intent)
     }
 
 }
---
+++ app/src/main/java/ge/sgurgenidze/stsertsvadze/messenger/registration/RegistrationActivity.kt
@@ -0,0 +1,14 @@
+package ge.sgurgenidze.stsertsvadze.messenger.registration
+
+import android.os.Bundle
+import ge.sgurgenidze.stsertsvadze.messenger.R
+import androidx.appcompat.app.AppCompatActivity
+
+class RegistrationActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_registration)
+    }
+
+}
